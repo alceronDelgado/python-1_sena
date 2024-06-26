73e32c704ba0897d9ba3f152fcf385c4d747efcf
alceronDelgado <luis_aceron@soy.sena.edu.co>
2024-06-26 15:13:02 -0500

    add exercices #2 and part from #3 on cuaderno de colección

--- cuaderno#3_colecciones/apropiacion_2_colecc.py
+++ cuaderno#3_colecciones/apropiacion_2_colecc.py
@@ -6,46 +6,27 @@
 
 #frase_uno = input("Digite una frase #1: ")
 #frase_dos = input("Digite una frase #2: ")
+frase_dos = "i Never Stop"
+frase_uno = "Circles"
 
+frase_uno = frase_uno.lower()
+frase_dos = frase_dos.lower()
 
-frase_uno = "memento mori"
-frase_dos = "love or die"
+min_letra = min(len(frase_uno), len(frase_dos))
+
+frase_uno = frase_uno[:min_letra]
+frase_dos = frase_dos[:min_letra]
+
+frase_uno = list(frase_uno)
+frase_dos = list(frase_dos)
 
 def frases(frase_uno,frase_dos):
-    lista_uno = []
-    lista_dos = []
-    
-    #Aplico el proceso con la frase #1
-    #Corto el espaciado de la cadena de texto, devuelve una lista
-    frase_uno = frase_uno.split(" ")
-    
-    #Como ya es una lista, uno los 2 elementos y queda una sola frase junta.
-    frase_uno = "".join(frase_uno)
-    
-    # Itinero cada letra para agregarlo a una lista1
-    for i in range(len(frase_uno)):
-        letra_1 = frase_uno[i]
-        lista_uno.append(letra_1)
 
-    #Aplico el proceso con la frase #2
-    frase_dos = frase_dos.split(" ")
-    frase_dos = "".join(frase_dos)
-    
-    # Itinero cada letra para agregarlo a la lista2
-    for k in range(len(frase_dos)):
-        letra_2 = frase_dos[k]
-        lista_dos.append(letra_2)
-    
-    
-    # Defino una lista
     letras_repetidas = []
-    # Itinero la primera y segunda lista para saber su rango. 
-    for j in lista_uno:
-        for o in lista_dos:
-            if j == o:
-                letras_repetidas.append(j)
-    #print(f"cantidad de palabras repetidas :{len(letras_repetidas)}")
-    print(f"{letras_repetidas}")
-
-frases(frase_uno, frase_dos)
+    for i in range(len(frase_uno)):
+        print(frase_uno)
+        if frase_uno[i] == frase_dos[i]:
+            letras_repetidas.append(frase_uno[i])
+    print(f"Las letras repetidas son : {letras_repetidas}")
         
+frases(frase_uno, frase_dos)
